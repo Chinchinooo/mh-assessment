72e3b642100f0a6f3bf3c56c22155db4cfe57612
Chinchinooo <chinchin081298@gmail.com>
2024-05-22 15:38:32 +0800

    added pagination css

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
+import './Pagination.css';
 
-const Pagination = ({ totalUsers, userPerPage, setCurrentPage }) => {
-    let pages = [];
-    for (let i = 1; i <= Math.ceil(totalUsers/userPerPage); i++) {
-        pages.push(i)
-    }  
+const Pagination = ({ totalUsers, userPerPage, setCurrentPage, currentPage }) => {
+  let pages = [];
+  for (let i = 1; i <= Math.ceil(totalUsers / userPerPage); i++) {
+    pages.push(i);
+  }
+
   return (
-    <div>
-        {pages.map((page, index) => {
-            return( 
-            <button 
+    <div className="pagination-container">
+      <div className="pagination">
+        {pages.map((page, index) => (
+          <div
             key={index}
+            className={`pagination-number ${currentPage === page ? 'active' : ''}`}
             onClick={() => setCurrentPage(page)}
-                >{page}
-            </button>
-        )})}
+          >
+            {page}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
-
 
 export default Pagination;
