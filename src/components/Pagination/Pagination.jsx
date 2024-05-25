661041abce76a346fd0b35617ca28513d28cf155
Chinchinooo <chinchin081298@gmail.com>
2024-05-25 19:59:41 +0800

    fix the pagination after changing to react context

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Pagination.css';
+import { AppContext } from '../../AppContext';
 
-const Pagination = ({ totalUsers, userPerPage, setCurrentPage, currentPage }) => {
+const Pagination = () => {
+  const { users, usersPerPage, setCurrentPage, currentPage } = useContext(AppContext);
+  
+    if (!users) {
+    return <div>Loading...</div>; 
+  }
+
+  let totalUsers = users.length
   let pages = [];
-  for (let i = 1; i <= Math.ceil(totalUsers / userPerPage); i++) {
+  for (let i = 1; i <= Math.ceil(totalUsers / usersPerPage); i++) {
     pages.push(i);
   }
+
+  console.log('pages:', pages);
+  console.log('totalUser:', totalUsers); // Log generated pages array
 
   return (
     <div className="pagination-container">
