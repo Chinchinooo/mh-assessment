43569609e04b6de1ecdaed6a0ecc5ea078403528
Chinchinooo <chinchin081298@gmail.com>
2024-05-26 00:18:28 +0800

    only shows the first user of users data (not dynamic)

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useContext } from 'react';
+import { AppContext } from '../../AppContext';
 
 const ProfilePage = () => {
+    const { users } = useContext(AppContext);
+
+    if (!users.length) {
+    return <p>Loading...</p>;
+    }
+
+    const firstUser = users[0];
+
     return(
         <div>
             <html>
@@ -8,53 +17,50 @@ const ProfilePage = () => {
                     <link rel="stylesheet" href="https://horizon-tailwind-react-git-tailwind-components-horizon-ui.vercel.app/static/css/main.ad49aa9b.css" />
                 </head>
                 <body >
-                    <div class="flex flex-col justify-center items-center h-[100vh]">
+                    <div class="flex flex-col justify-center items-center h-[85vh]">
                         <div class="relative flex flex-col items-center rounded-[20px] w-[700px] max-w-[95%] mx-auto bg-white bg-clip-border shadow-3xl shadow-shadow-500 dark:!bg-navy-800 dark:text-white dark:!shadow-none p-3">
                             <div class="mt-2 mb-8 w-full">
                                 <h4 class="px-2 text-xl font-bold text-navy-700 dark:text-white">
-                                General Information
+                                {firstUser.name}
                                 </h4>
-                                <p class="mt-2 px-2 text-base text-gray-600">
-                                As we live, our hearts turn colder. Cause pain is what we go through
-                                as we become older. We get insulted by others, lose trust for those
-                                others. We get back stabbed by friends. It becomes harder for us to
-                                give others a hand. We get our heart broken by people we love, even
-                                that we give them all...
+                                <p class="mt-2 px-2 text-base text-gray-600">Company : {firstUser.company.name}, <br></br> {firstUser.company.catchPhrase}, <br></br> {firstUser.company.bs}
                                 </p>
-                            </div> 
+                                <p class="mt-2 px-2 text-base text-gray-600">
+                                Address : {firstUser.address.street}, {firstUser.address.suite}, {firstUser.address.city}, {firstUser.address.zipcode}, geo: lat{firstUser.address.geo.lat}, long{firstUser.address.geo.lng}</p>
+                               
+                                        </div> 
                             <div class="grid grid-cols-2 gap-4 px-2 w-full">
                                 <div class="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
-                                <p class="text-sm text-gray-600">Education</p>
+                                <p class="text-sm text-gray-600">Username</p>
                                 <p class="text-base font-medium text-navy-700 dark:text-white">
-                                    Stanford University
+                                    {firstUser.username}
                                 </p>
                                 </div>
 
                                 <div class="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
-                                <p class="text-sm text-gray-600">Languages</p>
+                                <p class="text-sm text-gray-600">Email</p>
                                 <p class="text-base font-medium text-navy-700 dark:text-white">
-                                    English, Spanish, Italian
+                                    {firstUser.email}
                                 </p>
                                 </div>
-
                                 <div class="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
-                                <p class="text-sm text-gray-600">Department</p>
+                                <p class="text-sm text-gray-600">Phone</p>
                                 <p class="text-base font-medium text-navy-700 dark:text-white">
-                                    Product Design
+                                    {firstUser.phone}
                                 </p>
                                 </div>
 
                                 <div class="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
-                                <p class="text-sm text-gray-600">Work History</p>
+                                <p class="text-sm text-gray-600">Website</p>
                                 <p class="text-base font-medium text-navy-700 dark:text-white">
-                                    English, Spanish, Italian
+                                    {firstUser.website}
                                 </p>
                                 </div>
 
                                 <div class="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
-                                <p class="text-sm text-gray-600">Organization</p>
+                                <p class="text-sm text-gray-600">Company</p>
                                 <p class="text-base font-medium text-navy-700 dark:text-white">
-                                    Simmmple Web LLC
+                                    {firstUser.company.name}
                                 </p>
                                 </div>
 
@@ -66,7 +72,6 @@ const ProfilePage = () => {
                                 </div>
                             </div>
                         </div>  
-                        <p class="font-normal text-navy-700 mt-20 mx-auto w-max">Profile Card component from <a href="https://horizon-ui.com?ref=tailwindcomponents.com" target="_blank" class="text-brand-500 font-bold">Horizon UI Tailwind React</a></p>  
                     </div>
                 </body>
             </html>
