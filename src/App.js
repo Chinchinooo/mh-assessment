1debd1594d4f7395e029007cf8b2e59fb6cdafdb
Chinchinooo <chinchin081298@gmail.com>
2024-05-25 18:23:41 +0800

    added React Context and move all the state into the file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import Profile from './components/Profile/Profile.jsx'
 import ProfileList from './components/ProfileList/ProfileList.jsx';
@@ -6,17 +7,31 @@ import Pagination from './components/Pagination/Pagination.jsx';
 import Navigation from './components/Navigation/Navigation.jsx';
 import PostCard from './components/PostCard/PostCard.jsx';
 import PostCardList from './components/PostCardList/PostCardList.jsx';
+import PostwithComment from './components/PostwithComment/PostwithComment.jsx';
+import CommentsCard from './components/CommentsCard/CommentsCard.jsx';
 
 function App() {
   const [users, setUsers] = useState([]);
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [usersPerPage, setUsersPerPage] = useState(2);
 
    useEffect(() => {
     fetchUsers();
     fetchPosts();
+    fetchComments();
   }, []);
+
+  const fetchComments = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/comments');
+        const data = await response.json();
+        setComments(data);
+      } catch (error) {
+        console.error('Error fetching comments:', error);
+      }
+    };
 
   const fetchUsers = async () => {
     try {
@@ -47,14 +62,19 @@ function App() {
   return (
     <div className="App">
       <Navigation/>
-{/*       <Profile user={users[0]}/> 
+{/*   <Profile user={users[0]}/> 
+   
+      <PostComment
+        posts={posts}
+        users={users}/>
+        
+
       <ProfileList currentUsers={currentUsers}/>
       <Pagination
         totalUsers={users.length}
         userPerPage={usersPerPage}
-        setCurrentPage={setCurrentPage} 
-      />*/}
-      <PostCardList
+        setCurrentPage={setCurrentPage}/>*/}
+   <PostCardList
         posts={posts}
         users={users}
       />
---
+++ src/AppContext.js
@@ -0,0 +1,62 @@
+import React, {createContext, useState, useEffect} from "react";
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+    const [users, setUsers] = useState([]);
+    const [posts, setPosts] = useState([]);
+    const [comments, setComments] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [usersPerPage, setUsersPerPage] = useState(2);
+
+    useEffect(() => {
+        fetchUsers();
+        fetchPosts();
+        fetchComments();
+    }, []);
+
+const fetchComments = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/comments');
+        const data = await response.json();
+        setComments(data);
+      } catch (error) {
+        console.error('Error fetching comments:', error);
+      }
+    };
+
+  const fetchUsers = async () => {
+    try {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users");
+      const data = await response.json();
+      setUsers(data);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+
+  const fetchPosts = async () => {
+    try {
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+      const data = await response.json();
+      setPosts(data);
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
+  };
+
+const value = {
+    users,
+    posts,
+    comments,
+    currentPage,
+    setCurrentPage,
+    usersPerPage,
+    setUsersPerPage,
+  };
+
+  return <AppContext.Provider value={value}>{}</AppContext.Provider>;
+
+};
+
+export { AppContext, AppProvider };
