5980f78a56598a303b51b15804463e3ed863bc5d
Chinchinooo <chinchin081298@gmail.com>
2024-05-27 19:28:14 +0800

    fix the path

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Navigation from './components/Navigation/Navigation.jsx';
-import PostCard from './components/PostCard/PostCard.jsx';
 import PostCardList from './components/PostCardList/PostCardList.jsx';
-import CommentsCard from './components/CommentsCard/CommentsCard.jsx';
 import ProfilePage from './components/ProfilePage/ProfilePage.jsx';
 import Users from './pages/Users.jsx';
-import { AppProvider } from './AppContext.js';
+import CommentsList from './components/CommentsList/CommentList.jsx';
+import PostWithComments from './pages/PostWithComments.jsx';
+import DataCard from './components/DataCard/DataCard.jsx';
+import DashboardChart from './pages/DashboardChart.jsx';
 
 const App = () => {
   return (
@@ -15,8 +16,11 @@ const App = () => {
     <Navigation/>
     <Routes>
         <Route path='/' element={<PostCardList/>} />
+        <Route path='/posts' element={<PostCardList/>} />
+        <Route path='/posts/:id/comments' element={<PostWithComments/>} />
         <Route path='/users' element={<Users/>} />
         <Route path='/users/:id' element={<ProfilePage/>} />
+        <Route path='/dashboard' element={<DashboardChart/>} />
     </Routes>
     </div>
   );
