c05af4cd2ab5bd8ac3f8ba324468b3a60a0b2179
Chinchinooo <chinchin081298@gmail.com>
2024-05-27 23:50:39 +0800

    added basename for route mh-assessment

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { AppProvider } from './AppContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/mh-assessment'>
       <AppProvider>
         <App />
       </AppProvider>
