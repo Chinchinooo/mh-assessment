753486aa8d241748417e4c0c6d622801e05180eb
Chinchinooo <chinchin081298@gmail.com>
2024-05-27 13:36:05 +0800

    fix where calling api and filter the comments for post id

--- src/components/CommentsCard/CommentsCard.jsx
+++ src/components/CommentsCard/CommentsCard.jsx
@@ -6,15 +6,48 @@ const CommentsCard = ({comment}) => {
     }
 
     return(
-         <div className=" p-1">
-            <p>this is postid: {comment.postId}</p>
-            <p>this is name</p>
-            <h3>{comment.name}</h3>
-            <p>this is email</p>
-            <p>{comment.email}</p>
-            <p>{comment.body}</p>
+         <div>
+<div key={comment.id}>
+            <div className="mx-3 flex justify-center items-center">
+              <section className="w-full" style={{ maxWidth: '1000px' }}>
+                <div className="bg-white shadow-md mb-2 rounded-xl p-4 hover:bg-slate-100 transition duration-350 ease-in-out">
+                  <div className="flex-none lg:flex">
+                    <ul className="list-none">
+                      <li className="list-none">
+                        <div className="flex flex-shrink-0 p-4 pb-0">
+                          <a href="#" className="flex-shrink-0 group block">
+                            <div className="flex items-center">
+                              <div>
+                                <img className="inline-block h-10 w-10 rounded-full" src="https://pbs.twimg.com/profile_images/1121328878142853120/e-rpjoJi_bigger.png" alt="" />
+                              </div>
+                              <div className="ml-3">
+                                <p className="text-lg leading-6 font-medium text-black">
+                                  {comment.name}
+                                </p>
+                              </div>
+                            </div>
+                          </a>
+                        </div>
+
+                        <div className="pl-16">
+                          <p className="text-sm text-justify width-auto font-medium text-gray-600 flex-shrink pb-2">
+                            {comment.email}
+                          </p>
+                          <p className="text-justify width-auto font-medium text-gray-600 flex-shrink">
+                            {comment.body}
+                          </p>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div> 
         </div>
     )
 }
 
 export default CommentsCard;
+
+ 
--- src/components/CommentsList/CommentList.jsx
+++ src/components/CommentsList/CommentList.jsx
@@ -2,25 +2,22 @@ import React, { useContext } from 'react';
 import { AppContext } from '../../AppContext';
 import CommentsCard from '../CommentsCard/CommentsCard';
 
-const CommentsList = () => {
-    const {comments, posts} = useContext(AppContext);
+const CommentsList = ({selectedComment}) => {
+    const { posts } = useContext(AppContext);
 
-     if (!comments) {
+     if (!selectedComment) {
     return <p>Loading...</p>;
     }
    
     return(
          <div>
             {
-                comments.map((comment) => {
+                selectedComment.map((comment) => {
                     const post = posts.find((post) => post.id === comment.postId);
                    
                     return (
                         <CommentsCard
                             key={comment.id}
-                            post={post}
-                            posts={posts}
-                            comments={comments}
                             comment={comment}
                         />
                     )
